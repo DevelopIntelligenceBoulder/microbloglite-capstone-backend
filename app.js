9402cdef37ba66507cdc70b93b1bd2686341a2c8
Eric Schwartz <eric@developintelligence.com>
2022-11-15 22:29:59 -0500

    typo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = requre("cors")
+const cors = require("cors")
 
 const auth = require("./auth");
 
